c5d4c04d7b68094590e0178c1c6cca2e398705f5
ErikBoesen <me@erikboesen.com>
2019-07-26 23:22:56 -0400

    Fix emailing

--- app/email.py
+++ app/email.py
@@ -18,8 +18,8 @@ def send_email(subject, sender, recipients, text_body, html_body):
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email('[Microblog] Reset Your Password',
-               sender=app.config['ADMINS'][0],
+    send_email('[GroupMeBot] Reset Your Password',
+               sender='me@erikboesen.com',
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
                                          user=user, token=token),
