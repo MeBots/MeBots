2008950bd37313c94841a79adfe9c6796350deaf
ErikBoesen <me@erikboesen.com>
2019-07-28 11:15:38 -0400

    Show something if there are no bots to show

--- app/routes.py
+++ app/routes.py
@@ -18,7 +18,7 @@ def before_request():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     page = request.args.get('page', 1, type=int)
-    bots = Bot.query.all().paginate(page, app.config['ITEMS_PER_PAGE'], False)
+    bots = Bot.query.paginate(page, app.config['ITEMS_PER_PAGE'], False)
     next_url = url_for('index', page=bots.next_num) if bots.has_next else None
     prev_url = url_for('index', page=bots.prev_num) if bots.has_prev else None
     return render_template('index.html', title='Home',
