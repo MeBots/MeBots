9fb5594959e98bfe6291994bef8a010a4c0762c8
ErikBoesen <me@erikboesen.com>
2019-07-28 23:05:23 -0400

    Fully remove about me feature

--- app/routes.py
+++ app/routes.py
@@ -115,7 +115,6 @@ def edit_profile():
         return redirect(url_for('edit_profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
-        form.about_me.data = current_user.about_me
     return render_template('edit_profile.html',
                            title='Edit Profile',
                            form=form)
