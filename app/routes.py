564d31189c2e9856278f0bc684d2ffe8212dcd09
ErikBoesen <me@erikboesen.com>
2019-07-30 12:46:33 -0400

    Dump manager code

--- app/routes.py
+++ app/routes.py
@@ -169,7 +169,43 @@ def edit_bot(slug):
                            title='Edit bot',
                            form=form)
 
-"""
-@app.route('/manage/<bot_name>', methods=['GET', 'POST'])
-def manager
-"""
+
+@app.route("/manager/<slug>", methods=["GET", "POST"])
+def manager(slug):
+    access_token = request.args["access_token"]
+    callback_url = "https://botagainsthumanitygroupme.herokuapp.com/message"
+    me = requests.get(f"https://api.groupme.com/v3/users/me?token={access_token}").json()["response"]
+    if request.method == "POST":
+        # Build and send bot data
+        group_id = request.form["group_id"]
+        bot = {
+            "name": "Bot Against Humanity",
+            "group_id": group_id,
+            "avatar_url": "https://i.groupme.com/200x200.png.092e3648ee2745aeb3296a51b3a85e0f",
+            "callback_url": callback_url,
+        }
+        result = requests.post(f"https://api.groupme.com/v3/bots?token={access_token}",
+                               json={"bot": bot}).json()["response"]["bot"]
+        group = requests.get(f"https://api.groupme.com/v3/groups/{group_id}?token={access_token}").json()["response"]
+
+        # Store in database
+        registrant = Bot(group_id, group["name"], result["bot_id"], me["user_id"], me["name"], access_token)
+        db.session.add(registrant)
+        db.session.commit()
+    groups = requests.get(f"https://api.groupme.com/v3/groups?token={access_token}").json()["response"]
+    groups = [group for group in groups if not Bot.query.get(group["group_id"])]
+    groupme_bots = requests.get(f"https://api.groupme.com/v3/bots?token={access_token}").json()["response"]
+    bots = Bot.query.filter_by(owner_id=me["user_id"])
+    return render_template("manager.html", access_token=access_token, groups=groups, bots=bots)
+
+
+@app.route("/delete", methods=["POST"])
+def delete_bot():
+    data = request.get_json()
+    access_token = data["access_token"]
+    bot = Bot.query.get(data["group_id"])
+    req = requests.post(f"https://api.groupme.com/v3/bots/destroy?token={access_token}", json={"bot_id": bot.bot_id})
+    if req.ok:
+        db.session.delete(bot)
+        db.session.commit()
+        return "ok", 200
