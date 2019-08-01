0ce36e7bcea61ae076acc9972dc18c2b32f56460
ErikBoesen <me@erikboesen.com>
2019-07-31 20:31:09 -0400

    Set CLIENT_ID in config

--- config.py
+++ config.py
@@ -12,6 +12,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    CLIENT_ID = ''
+    CLIENT_ID = 'ZdSzatBHBT06cjk100Bvk4RHuPCyKy53FQFpnnnHZFbPu9lB'
     ITEMS_PER_PAGE = 8
     IMAGE_ROOT = 'https://i.groupme.com/'
