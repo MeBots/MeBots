43ad2e5c557f1b3ab1d1ff13584a11d255214315
ErikBoesen <me@erikboesen.com>
2019-07-31 23:27:27 -0400

    Add starting avatar script

---
+++ app/static/js/main.js
@@ -0,0 +1,9 @@
+// There is no elegance here. Only sleep deprivation and regret.
+let avatarInput = document.getElementById("avatar_url"), avatarPreview;
+function previewAvatar() {
+    avatarPreview.style.backgroundImage = "url(" + avatarInput.value + ")";
+}
+if (avatarInput) {
+    avatarPreview = document.createElement("avatar_preview");
+    previewAvatar();
+    avatarInput.oninput = previewAvatar;
