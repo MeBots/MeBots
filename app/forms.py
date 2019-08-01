b03331aa7fa7d6400aa2c2693044fc4e759bd1b0
ErikBoesen <me@erikboesen.com>
2019-07-31 22:53:48 -0400

    Delete edit profile form

--- app/forms.py
+++ app/forms.py
@@ -4,21 +4,6 @@ from wtforms.validators import ValidationError, DataRequired, Length
 from app.models import User
 
 
-class EditProfileForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
-    def __init__(self, original_username, *args, **kwargs):
-        super(EditProfileForm, self).__init__(*args, **kwargs)
-        self.original_username = original_username
-
-    def validate_username(self, username):
-        if username.data != self.original_username:
-            user = User.query.filter_by(username=self.username.data).first()
-            if user is not None:
-                raise ValidationError('Please use a different username.')
-
-
 class BotForm(FlaskForm):
     name = StringField('Bot name', validators=[DataRequired()])
     slug = StringField('Shortname', validators=[DataRequired()])
