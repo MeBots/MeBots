72c710b87f8825f96aee0cacb7cbdf23af1b2df6
ErikBoesen <me@erikboesen.com>
2019-09-21 02:57:08 -0400

    URL validation

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, TextAreaField, BooleanField, SelectField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Length
+from wtforms.validators import ValidationError, DataRequired, Length, URL
 from app.models import User
 
 
@@ -8,14 +8,14 @@ class BotForm(FlaskForm):
     name = StringField('Bot name', validators=[DataRequired(), Length(max=32)])
     slug = StringField('Shortname', validators=[DataRequired(), Length(max=32)])
     name_customizable = BooleanField('Allow customizing name')
-    avatar_url = StringField('Avatar URL')
+    avatar_url = StringField('Avatar URL', validators=[URL()])
     avatar_url_customizable = BooleanField('Allow customizing avatar')
-    callback_url = StringField('Callback URL', validators=[DataRequired(), Length(max=128)])
+    callback_url = StringField('Callback URL', validators=[DataRequired(), Length(max=128), URL()])
     description = TextAreaField('Description', validators=[Length(max=200)])
-    website = StringField('Website', validators=[Length(max=128)])
+    website = StringField('Website', validators=[Length(max=128), URL()])
     prefix = StringField('Prefix', validators=[Length(max=20)])
-    test_group = StringField('Link for testing/informational group', validators=[Length(max=60)])
-    repo = StringField('GitHub/source code repository', validators=[Length(max=100)])
+    test_group = StringField('Link for testing/informational group', validators=[Length(max=60), URL()])
+    repo = StringField('GitHub/source code repository', validators=[Length(max=100), URL()])
 
     submit = SubmitField('Submit')
 
