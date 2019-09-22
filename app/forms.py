2f157ca196bf74559c7b43461650be5a627dbee0
ErikBoesen <me@erikboesen.com>
2019-09-22 16:32:16 -0400

    Remove URL requirement from unrequired fields in bot creation form

--- app/forms.py
+++ app/forms.py
@@ -8,14 +8,14 @@ class BotForm(FlaskForm):
     name = StringField('Bot name', validators=[DataRequired(), Length(max=32)])
     slug = StringField('Shortname', validators=[DataRequired(), Length(max=32)])
     name_customizable = BooleanField('Allow customizing name')
-    avatar_url = StringField('Avatar URL', validators=[URL()])
+    avatar_url = StringField('Avatar URL', validators=[])
     avatar_url_customizable = BooleanField('Allow customizing avatar')
     callback_url = StringField('Callback URL', validators=[DataRequired(), Length(max=128), URL()])
     description = TextAreaField('Description', validators=[Length(max=1000)])
-    website = StringField('Website', validators=[Length(max=128), URL()])
+    website = StringField('Website', validators=[Length(max=128)])
     prefix = StringField('Command prefix', validators=[Length(max=20)])
-    test_group = StringField('Link for testing/informational group', validators=[Length(max=60), URL()])
-    repo = StringField('GitHub/source code repository', validators=[Length(max=100), URL()])
+    test_group = StringField('Link for testing/informational group', validators=[Length(max=60)])
+    repo = StringField('GitHub/source code repository', validators=[Length(max=100)])
 
     submit = SubmitField('Submit')
 
