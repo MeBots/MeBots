7dd8a4c1db691cd99629c846ef9417970458f9f0
ErikBoesen <me@erikboesen.com>
2020-04-03 18:39:39 -0400

    Remove mail

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_mail import Mail
 from config import Config
 
 app = Flask(__name__)
@@ -14,7 +13,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-mail = Mail(app)
 
 if not app.debug:
     if not os.path.exists('logs'):
