fe3450fccc4da6d32fc62c126d04000fc9d35c78
ErikBoesen <me@erikboesen.com>
2020-09-07 13:01:49 -0600

    DRY API requests

--- app/api.py
+++ app/api.py
@@ -1,26 +1,27 @@
 from flask import Blueprint, jsonify, request
 from app import db
-from app.models import Bot, Instance
+from app.models import User, Bot, Instance
 
 
 api_blueprint = Blueprint('api', __name__)
 
 
-@api_blueprint.route('/bot/<slug>')
-def api_bot(slug):
-    # TODO: use a respectable 404
+@api_blueprint.before_request
+def check_auth():
     token = request.args.get("token")
     if not token or token != bot.token:
         return {"error": "Missing or invalid token."}, 401
+
+
+@api_blueprint.route('/bot/<slug>')
+def api_bot(slug):
+    # TODO: use a respectable 404
     bot = Bot.query.filter_by(slug=slug).first_or_404()
     return bot.json()
 
 
 @api_blueprint.route('/bot/<slug>/instance/<group_id>')
 def api_instance(slug, group_id):
-    token = request.args.get("token")
     bot = Bot.query.filter_by(slug=slug).first_or_404()
-    if not token or token != bot.token:
-        return {"error": "Missing or invalid token."}
     instance = Instance.query.filter_by(bot_id=bot.id, group_id=group_id).first_or_404()
     return {"id": instance.id}
