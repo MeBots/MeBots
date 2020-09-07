6c19a24b8a7f68bcfe24bc8749ced2416ca6d26f
ErikBoesen <me@erikboesen.com>
2020-09-07 13:16:34 -0600

    Undo DRYing

--- app/api.py
+++ app/api.py
@@ -6,22 +6,21 @@ from app.models import User, Bot, Instance
 api_blueprint = Blueprint('api', __name__)
 
 
-@api_blueprint.before_request
-def check_auth():
+@api_blueprint.route('/bot/<slug>')
+def api_bot(slug):
     token = request.args.get("token")
     g.bot = Bot.query.filter_by(slug=slug).first_or_404()
     if not token or token != g.bot.token:
         return {"error": "Missing or invalid token."}, 401
-
-
-@api_blueprint.route('/bot/<slug>')
-def api_bot(slug):
-    # TODO: use a respectable 404
     return g.bot.json()
 
 
 @api_blueprint.route('/bot/<slug>/instance/<group_id>')
 def api_instance(slug, group_id):
+    token = request.args.get("token")
+    g.bot = Bot.query.filter_by(slug=slug).first_or_404()
+    if not token or token != g.bot.token:
+        return {"error": "Missing or invalid token."}, 401
     instance = Instance.query.filter_by(bot_id=g.bot.id, group_id=group_id).first_or_404()
     json = {"id": instance.id}
     if g.bot.has_user_token_access:
