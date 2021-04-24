4b7181f5974b42c567aa9b2e72fa3f8ec748f8cb
ErikBoesen <me@erikboesen.com>
2021-04-24 13:29:08 -0400

    Fix undefined send_from_directory

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 import requests
-from flask import render_template, flash, redirect, url_for, request, abort, make_response
+from flask import render_template, flash, redirect, url_for, request, abort, make_response, send_from_directory
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
