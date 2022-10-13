9ca2348dcd49558f2ce3418b6e36a1f6525f8232
ErikBoesen <me@erikboesen.com>
2022-10-13 15:30:10 -0400

    Add util.py

---
+++ app/util.py
@@ -0,0 +1,53 @@
+from flask import jsonify
+from sqlalchemy.ext.declarative import DeclarativeMeta
+
+import datetime
+import json
+
+
+class ModelEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj.__class__, DeclarativeMeta):
+            # go through each field in this SQLalchemy class
+            fields = {}
+            for field in obj.__class__.__serializable__:
+                val = obj.__getattribute__(field)
+
+                # is this field another SQLalchemy object, or a list of SQLalchemy objects?
+                if isinstance(val.__class__, DeclarativeMeta) or (isinstance(val, list) and len(val) > 0 and isinstance(val[0].__class__, DeclarativeMeta)):
+                    # unless we're expanding this field, stop here
+                    if field not in obj.__class__._to_expand:
+                        # not expanding this field: set it to None and continue
+                        fields[field] = None
+                        continue
+                fields[field] = val
+            # a json-encodable dict
+            return fields
+
+        return json.JSONEncoder.default(self, obj)
+
+
+def to_json(model):
+    return json.dumps(model, cls=ModelEncoder)
+
+
+# HTTP response generators
+
+def succ(message, code=200):
+    return (
+        jsonify({
+            'status': 'success',
+            'message': message,
+        }),
+        code
+    )
+
+
+def fail(message, code=400):
+    return (
+        jsonify({
+            'status': 'fail',
+            'message': message,
+        }),
+        code
+    )
