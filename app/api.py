4cf3b8bf7e5309c96dab13dcef449275ba56ee62
ErikBoesen <me@erikboesen.com>
2022-10-13 15:30:24 -0400

    Add initial receive endpoint

--- app/api.py
+++ app/api.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request, g
+import requests
 from app import db
 from app.models import User, Bot, Instance
+from app.util import succ, fial,
 
 
 api_blueprint = Blueprint('api', __name__)
@@ -10,10 +12,10 @@ api_blueprint = Blueprint('api', __name__)
 # Legacy
 @api_blueprint.route('/bot/<slug>')
 def api_bot(slug):
-    token = request.args.get("token")
+    token = request.args.get('token')
     g.bot = Bot.query.filter_by(slug=slug).first_or_404()
     if not token or token != g.bot.token:
-        return {"error": "Missing or invalid token."}, 401
+        return fail('Missing or invalid token.', 401)
     return g.bot.json()
 
 
@@ -29,12 +31,23 @@ def api_instances(slug):
 # Legacy
 @api_blueprint.route('/bot/<slug>/instance/<group_id>')
 def api_instance(slug, group_id):
-    token = request.args.get("token")
+    token = request.args.get('token')
     g.bot = Bot.query.filter_by(slug=slug).first_or_404()
     if not token or token != g.bot.token:
-        return {"error": "Missing or invalid token."}, 401
+        return fail('Missing or invalid token.', 401)
     instance = Instance.query.filter_by(bot_id=g.bot.id, group_id=group_id).first_or_404()
-    json = {"id": instance.id}
+    json = {'id': instance.id}
     if g.bot.has_user_token_access:
-        json["token"] = User.query.get(instance.owner_id).token
+        json['token'] = User.query.get(instance.owner_id).token
     return jsonify(json)
+
+
+# Receiving messages
+@api_blueprint.route('/bots/<slug>/messages', methods=['POST'])
+def api_bot_receive(slug):
+    g.bot = Bot.query.filter_by(slug=slug).first_or_404()
+    payload = request.get_json()
+    print('Received message; payload:')
+    print(payload)
+    forward = requests.post(g.bot.callback_url, json=payload)
+    return succ('Message processed.', 202)
