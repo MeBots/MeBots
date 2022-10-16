b70ed408dcd152bb20bf6210fa350885f553478b
ErikBoesen <me@erikboesen.com>
2022-10-16 14:19:09 -0400

    Add some placeholders and descriptions for bot form

--- app/forms.py
+++ app/forms.py
@@ -10,19 +10,20 @@ class BotForm(FlaskForm):
     name_customizable = BooleanField('Allow customizing name')
     avatar_url = StringField('Avatar URL', validators=[])
     avatar_url_customizable = BooleanField('Allow customizing avatar')
-    callback_url = StringField('Callback URL', validators=[Optional(), Length(max=128), URL()])
+    callback_url = StringField('Callback URL', validators=[Optional(), Length(max=128), URL()], description='Publicly accessible URL where your bot server can receive message POST requests.')
     description = TextAreaField('Description', validators=[Length(max=1000)])
     website = StringField('Website', validators=[Length(max=128)])
-    prefix = StringField('Command prefix', validators=[Length(max=20)])
-    test_group = StringField('Link for testing/informational group', validators=[Length(max=60)])
-    repo = StringField('GitHub/source code repository', validators=[Length(max=100)])
+    prefix = StringField('Command prefix', validators=[Length(max=20)], render_kw={'placeholder': '/, !, #, etc.'})
+    prefix_filter = BooleanField('Ignore messages without prefix', validators=[Length(max=20)], description='Prevent receipt of messages that don\'t query your bot. May help save on hosting costs.')
+    test_group = StringField('Link to join testing/informational group', validators=[Length(max=60)], render_kw={'placeholder': 'groupme.com/join_group/abcdefgh/12345678'})
+    repo = StringField('Source code repository', validators=[Length(max=100)], render_kw={'placeholder': 'github.com/Dave/Hal9000'})
 
     submit = SubmitField('Submit')
 
 
 class InstanceForm(FlaskForm):
     group_id = SelectField('Group')
-    name = StringField('Bot name')
-    avatar_url = StringField('Bot avatar')
+    name = StringField('Custom name')
+    avatar_url = StringField('Custom avatar')
 
     submit = SubmitField('Add bot')
