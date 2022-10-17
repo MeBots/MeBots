ecdb8b5b5b209f71e7247d818f6f145b60e42f49
ErikBoesen <me@erikboesen.com>
2022-10-17 01:53:42 -0400

    Implement searching

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -1,1 +1,29 @@
-/* DON'T LOOK AT ME */
+/* There is no elegance here. Only sleep deprivation and regret. */
+
+const bots = document.getElementsByClassName('bot');
+let strings = [];
+for (let bot of bots) {
+    let name = bot.getElementsByTagName('h4')[0];
+    let description = bot.querySelector('.description');
+    let string = name.textContent
+    if (description) {
+        string += ' ' + description.textContent;
+    }
+    string = string.toLowerCase();
+    strings.push(string);
+}
+
+let searchBar = document.getElementById('search_bar');
+searchBar.oninput = function() {
+    let terms = searchBar.value.toLowerCase().split(' ').filter((term) => term);
+    console.log(terms);
+    for (let ind = 0; ind < strings.length; ind++) {
+        for (let term in terms) {
+            if (!strings[ind].includes(term)) {
+                bots[ind].style.display = 'none';
+                break;
+            }
+            bots[ind].style.display = 'block';
+        }
+    }
+};
