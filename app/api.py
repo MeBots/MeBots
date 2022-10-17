4ca8afb1dcbec13a737998108a79ef480375ef8c
ErikBoesen <me@erikboesen.com>
2022-10-17 13:14:29 -0400

    Remove unnecessary log

--- app/api.py
+++ app/api.py
@@ -58,7 +58,6 @@ def api_instances(slug):
 def api_instance(slug, group_id):
     token = request.args.get('token')
     g.bot = Bot.query.filter_by(slug=slug).first_or_404()
-    print(token)
     if not token or token != g.bot.token:
         return fail('Missing or invalid token.', 401)
     instance = Instance.query.filter_by(bot_id=g.bot.id, group_id=group_id).first_or_404()
