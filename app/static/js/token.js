202772ddf0f28683bf47a5dc2ec7c057f3039cbc
ErikBoesen <me@erikboesen.com>
2022-10-16 20:15:05 -0400

    Fix missing token

--- app/static/js/token.js
+++ app/static/js/token.js
@@ -1,6 +1,6 @@
-const readout_token = document.getElementById("readout_token"),
-      copy_token = document.getElementById("copy_token"),
-      reset_token = document.getElementById("reset_token");
+const readout_token = document.getElementById('readout_token'),
+      copy_token = document.getElementById('copy_token'),
+      reset_token = document.getElementById('reset_token');
 
 
 copy_token.onclick = function(e) {
@@ -10,10 +10,10 @@ copy_token.onclick = function(e) {
 
 reset_token.onclick = function(e) {
     var req = new XMLHttpRequest();
-    req.open("POST", "/reset_token");
-    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.open('POST', '/reset_token');
+    req.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
     req.send(JSON.stringify({
-        "slug": e.target.getAttribute("slug"),
+        'slug': e.target.getAttribute('slug'),
     }));
     req.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
