ac6799665cd7bcb61891acdd52cdb1138ea22bd6
ErikBoesen <me@erikboesen.com>
2022-11-12 15:31:29 -0500

    Start generating emails in notify_transition script

--- notify_transition.py
+++ notify_transition.py
@@ -1,11 +1,16 @@
 from app.models import Bot, Instance
 
-slug = input('Bot slug to send warnings for: ')
-
-bot = Bot.query.filter_by(slug=slug).first()
+bot = Bot.query.filter(Bot.slug.in_(slugs).first()
 instances = Instance.query.filter_by(bot_id=bot.id, created=None).all()
+groups = {}
 for instance in instances:
-    message = f'Due to a GroupMe update, this bot needs to be reset to keep working. The bot owner, {instance.owner.name}, will need to log in at https://mebots.io/bot/{bot.slug} and it will be automatically regenerated.'
+    if instance.owner not in groups:
+        groups[instance.owner] = []
+    groups[email].append(instance.group_name)
+    #message = f'> Due to a GroupMe update, this bot needs to be reset to keep working. The bot owner, {instance.owner.name}, will need to log in at https://mebots.io/bot/{bot.slug} and it will be automatically repaired.'
     #instance.send_message(message)
-    print(message)
 
+for user, group_names in groups.items():
+    name = user.split()[0]
+    groups_phrase = group_names.join('
+    message = f'Hello {name}!\n\nDue to a GroupMe update, some your bots from MeBots in some of your groups, including  need to be reset to keep working. This includes
