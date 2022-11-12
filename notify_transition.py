e388969f518aed623ca83b1a941999a087b7b64f
ErikBoesen <me@erikboesen.com>
2022-11-11 23:28:51 -0500

    Use slug in notify transition script

--- notify_transition.py
+++ notify_transition.py
@@ -1,14 +1,11 @@
 from app.models import Bot, Instance
-import argparse
 
-argument_parser = ArgumentParser
+slug = input('Bot slug to send warnings for: ')
 
-bot_id = int(input('Bot ID to send warnings for: '))
-
-bot = Bot.query.filter_by(slug=slug)
+bot = Bot.query.filter_by(slug=slug).first()
 instances = Instance.query.filter_by(bot_id=bot.id, created=None).all()
 for instance in instances:
-    message = f'Due to a GroupMe update, this bot needs to be reset to continue working. The bot owner, {instance.owner.name}, will need to log in at https://mebots.io/bot/{bot.slug} and the bot will be automatically regenerated. Thanks!'
+    message = f'Due to a GroupMe update, this bot needs to be reset to keep working. The bot owner, {instance.owner.name}, will need to log in at https://mebots.io/bot/{bot.slug} and it will be automatically regenerated.'
+    #instance.send_message(message)
     print(message)
-
 
