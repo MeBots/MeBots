224080950f68da71f2698b673c7a05a28ab05009
ErikBoesen <me@erikboesen.com>
2022-11-14 21:50:46 -0500

    Add email config

--- config.py
+++ config.py
@@ -10,3 +10,13 @@ class Config(object):
     CLIENT_ID = os.environ.get('CLIENT_ID')
     ITEMS_PER_PAGE = 256
     IMAGE_ROOT = 'https://i.groupme.com/'
+
+    # Email sending with Gmail
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    # Authentication
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = 'team@mebots.io'
