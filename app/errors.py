26361621271c6add06b4f77a3bb72b04766c6868
ErikBoesen <me@erikboesen.com>
2024-01-29 15:36:42 -0500

    Add error messages in errors.py

--- app/errors.py
+++ app/errors.py
@@ -4,15 +4,15 @@ from app import app, db
 
 @app.errorhandler(401)
 def not_found_error(error):
-    return render_template('401.html'), 401
+    return render_template('error.html', message='You don\'t have access to this page.'), 401
 
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html'), 404
+    return render_template('error.html', message='Not found.'), 404
 
 
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('error.html', message='An unexpected error has occurred.'), 500
